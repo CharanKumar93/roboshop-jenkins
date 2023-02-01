c4327bade211f7f98db32ab6304590263b4b86a2
Charan <gcharankumar99@gmail.com>
2023-02-01 14:51:11 -0600

    vars file

--- update-dns-record.sh
+++ update-dns-record.sh
@@ -11,7 +11,7 @@ echo '
       "TTL": 15,
       "ResourceRecords": [{ "Value": "IPADDRESS"}]
     }}]
-}' | sed -e "s/IpAddress/${IP}/" ./tmp/jenkins.json
+}' | sed -e "s/IPADDRESS/${IP}/" >/tmp/jenkins.json
 
 ZONE_ID="Z01921203I3IC6L44J4QQ"
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/jenkins.json | jq .
